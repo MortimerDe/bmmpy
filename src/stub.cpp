ac7514c65fa6aa07ddd94c716241c1fb1d96915c
MortimerDe <tomas.howard.suffolk@gmail.com>
2026-04-03 14:45:58 +0800

    format code style in .clang-format

--- src/stub.cpp
+++ src/stub.cpp
@@ -1,7 +1,13 @@
 #include "bmmpy/stub.hpp"
 
-namespace bmmpy
-{
-    std::string get_version() { return BMMPY_VERSION; }
-    int add(int a, int b) { return a + b; }
+namespace bmmpy {
+
+std::string get_version() {
+    return BMMPY_VERSION;
 }
+
+int add(int a, int b) {
+    return a + b;
+}
+
+} // namespace bmmpy
