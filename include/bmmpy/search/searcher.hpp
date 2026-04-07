c03b02c5bb90442a180c4b7b8933bbb6a80c24b6
MortimerDe <tomas.howard.suffolk@gmail.com>
2026-04-07 12:51:07 +0800

    add describe method to searchers

--- include/bmmpy/search/searcher.hpp
+++ include/bmmpy/search/searcher.hpp
@@ -15,6 +15,10 @@ public:
                                           const std::vector<std::size_t>& window_rows) = 0;
     virtual const char* name() const noexcept = 0;
 
+    virtual std::string describe(std::size_t window_size) const {
+        return std::string(name()) + "" + std::to_string(window_size);
+    }
+
 private:
 };
 
