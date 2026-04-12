9e6a91b541d2b1bc3154298c2f5302d99e8579b5
MortimerDe <tomas.howard.suffolk@gmail.com>
2026-04-13 06:45:44 +0800

    update fwht16 tests to include cpu backends

--- src/fwht16/engine.cpp
+++ src/fwht16/engine.cpp
@@ -7,6 +7,7 @@
 
 namespace bmmpy::fwht16 {
 namespace {
+
 void validate_request(const Fwht16BatchRequest& request) {
     if (request.batch_size != 0 && request.samples == nullptr) {
         throw std::invalid_argument(
@@ -17,6 +18,7 @@ void validate_request(const Fwht16BatchRequest& request) {
         throw std::invalid_argument("Fwht16Engine: topk must be non-zero in topk mode");
     }
 }
+
 } // namespace
 
 Fwht16BatchResponse Fwht16Engine::run(const Fwht16BatchRequest& request) const {
@@ -25,18 +27,20 @@ Fwht16BatchResponse Fwht16Engine::run(const Fwht16BatchRequest& request) const {
     switch (request.backend) {
     case Fwht16Backend::cpu:
         return CpuFwht16Executor{}.run(request);
+
     case Fwht16Backend::gpu:
         if (!GpuFwht16Executor::available()) {
             throw std::runtime_error("Fwht16Engine: GPU backend requested but not available");
         }
         return GpuFwht16Executor{}.run(request);
+
     case Fwht16Backend::auto_select:
         if (GpuFwht16Executor::available()) {
             return GpuFwht16Executor{}.run(request);
-        } else {
-            return CpuFwht16Executor{}.run(request);
         }
+        return CpuFwht16Executor{}.run(request);
     }
+
     throw std::invalid_argument("Fwht16Engine: unknown backend specified");
 }
 } // namespace bmmpy::fwht16
--- tests/fwht16_tests.cpp
+++ tests/fwht16_tests.cpp
@@ -37,7 +37,7 @@ void test_constants_are_stable() {
     require(bmmpy::fwht16::Fwht16Constants::k_max_weight == 512, "k_max_weight");
 }
 
-void test_cpu_stub_returns_empty_topk_layout() {
+void test_cpu_scalar_stub_returns_fixed_topk_layout() {
     bmmpy::fwht16::ColumnMasks16 sample{};
     bmmpy::fwht16::Fwht16Engine engine;
 
@@ -45,19 +45,44 @@ void test_cpu_stub_returns_empty_topk_layout() {
     request.samples = &sample;
     request.batch_size = 1;
     request.backend = bmmpy::fwht16::Fwht16Backend::cpu;
+    request.cpu_backend = bmmpy::fwht16::Fwht16CpuBackend::scalar;
     request.mode = bmmpy::fwht16::Fwht16ResultMode::topk;
     request.topk = 8;
 
     const auto response = engine.run(request);
 
     require(response.actual_backend == bmmpy::fwht16::Fwht16Backend::cpu, "actual_backend");
+    require(response.actual_cpu_backend == bmmpy::fwht16::Fwht16CpuBackend::scalar,
+            "actual_cpu_backend");
     require(response.mode == bmmpy::fwht16::Fwht16ResultMode::topk, "mode");
     require(response.batch_size == 1, "batch_size");
-    require(response.topk_offsets.size() == 2, "topk_offsets size");
-    require(response.topk_offsets[0] == 0, "topk_offsets[0]");
-    require(response.topk_offsets[1] == 0, "topk_offsets[1]");
-    require(response.topk_results.empty(), "topk_results empty");
+    require(response.topk_results.size() == 8, "topk_results size");
     require(response.spectra.empty(), "spectra empty");
+
+    for (const auto& item : response.topk_results) {
+        require(item.mask == 0, "topk mask zero initialized");
+        require(item.weight == 0, "topk weight zero initialized");
+    }
+}
+
+void test_cpu_auto_stub_defaults_to_scalar() {
+    bmmpy::fwht16::ColumnMasks16 sample{};
+    bmmpy::fwht16::Fwht16Engine engine;
+
+    bmmpy::fwht16::Fwht16BatchRequest request;
+    request.samples = &sample;
+    request.batch_size = 1;
+    request.backend = bmmpy::fwht16::Fwht16Backend::cpu;
+    request.cpu_backend = bmmpy::fwht16::Fwht16CpuBackend::auto_select;
+    request.mode = bmmpy::fwht16::Fwht16ResultMode::topk;
+    request.topk = 4;
+
+    const auto response = engine.run(request);
+
+    require(response.actual_backend == bmmpy::fwht16::Fwht16Backend::cpu, "auto actual_backend");
+    require(response.actual_cpu_backend == bmmpy::fwht16::Fwht16CpuBackend::scalar,
+            "auto actual_cpu_backend");
+    require(response.topk_results.size() == 4, "auto topk_results size");
 }
 
 void test_cpu_stub_returns_zeroed_spectrum_layout() {
@@ -68,15 +93,17 @@ void test_cpu_stub_returns_zeroed_spectrum_layout() {
     request.samples = samples;
     request.batch_size = 2;
     request.backend = bmmpy::fwht16::Fwht16Backend::cpu;
+    request.cpu_backend = bmmpy::fwht16::Fwht16CpuBackend::scalar;
     request.mode = bmmpy::fwht16::Fwht16ResultMode::spectrum;
 
     const auto response = engine.run(request);
 
     require(response.actual_backend == bmmpy::fwht16::Fwht16Backend::cpu,
             "spectrum actual_backend");
+    require(response.actual_cpu_backend == bmmpy::fwht16::Fwht16CpuBackend::scalar,
+            "spectrum actual_cpu_backend");
     require(response.mode == bmmpy::fwht16::Fwht16ResultMode::spectrum, "spectrum mode");
     require(response.batch_size == 2, "spectrum batch_size");
-    require(response.topk_offsets.empty(), "spectrum topk_offsets empty");
     require(response.topk_results.empty(), "spectrum topk_results empty");
     require(response.spectra.size() == 2 * bmmpy::fwht16::Fwht16Constants::k_spectrum_size,
             "spectra size");
@@ -113,21 +140,27 @@ void test_engine_cpu_route_uses_cpu_backend() {
     request.samples = &sample;
     request.batch_size = 1;
     request.backend = bmmpy::fwht16::Fwht16Backend::cpu;
+    request.cpu_backend = bmmpy::fwht16::Fwht16CpuBackend::scalar;
 
     const auto response = engine.run(request);
     require(response.actual_backend == bmmpy::fwht16::Fwht16Backend::cpu, "cpu route");
+    require(response.actual_cpu_backend == bmmpy::fwht16::Fwht16CpuBackend::scalar,
+            "cpu route backend");
 }
 
-void test_engine_auto_route_falls_back_to_cpu() {
+void test_engine_auto_route_falls_back_to_cpu_scalar() {
     bmmpy::fwht16::ColumnMasks16 sample{};
     bmmpy::fwht16::Fwht16Engine engine;
     bmmpy::fwht16::Fwht16BatchRequest request;
     request.samples = &sample;
     request.batch_size = 1;
     request.backend = bmmpy::fwht16::Fwht16Backend::auto_select;
+    request.cpu_backend = bmmpy::fwht16::Fwht16CpuBackend::auto_select;
 
     const auto response = engine.run(request);
     require(response.actual_backend == bmmpy::fwht16::Fwht16Backend::cpu, "auto route");
+    require(response.actual_cpu_backend == bmmpy::fwht16::Fwht16CpuBackend::scalar,
+            "auto cpu route");
 }
 
 void test_engine_gpu_route_throws_when_unavailable() {
@@ -141,6 +174,30 @@ void test_engine_gpu_route_throws_when_unavailable() {
     expect_throw<std::runtime_error>([&] { (void)engine.run(request); }, "gpu unavailable");
 }
 
+void test_cpu_avx2_route_throws_when_unimplemented() {
+    bmmpy::fwht16::ColumnMasks16 sample{};
+    bmmpy::fwht16::Fwht16Engine engine;
+    bmmpy::fwht16::Fwht16BatchRequest request;
+    request.samples = &sample;
+    request.batch_size = 1;
+    request.backend = bmmpy::fwht16::Fwht16Backend::cpu;
+    request.cpu_backend = bmmpy::fwht16::Fwht16CpuBackend::avx2;
+
+    expect_throw<std::runtime_error>([&] { (void)engine.run(request); }, "avx2 unavailable");
+}
+
+void test_cpu_avx512_route_throws_when_unimplemented() {
+    bmmpy::fwht16::ColumnMasks16 sample{};
+    bmmpy::fwht16::Fwht16Engine engine;
+    bmmpy::fwht16::Fwht16BatchRequest request;
+    request.samples = &sample;
+    request.batch_size = 1;
+    request.backend = bmmpy::fwht16::Fwht16Backend::cpu;
+    request.cpu_backend = bmmpy::fwht16::Fwht16CpuBackend::avx512;
+
+    expect_throw<std::runtime_error>([&] { (void)engine.run(request); }, "avx512 unavailable");
+}
+
 struct TestCase {
     const char* name;
     void (*fn)();
@@ -151,15 +208,22 @@ struct TestCase {
 int main() {
     const TestCase tests[] = {
         {"constants_are_stable", &test_constants_are_stable},
-        {"cpu_stub_returns_empty_topk_layout", &test_cpu_stub_returns_empty_topk_layout},
+        {"cpu_scalar_stub_returns_fixed_topk_layout",
+         &test_cpu_scalar_stub_returns_fixed_topk_layout},
+        {"cpu_auto_stub_defaults_to_scalar", &test_cpu_auto_stub_defaults_to_scalar},
         {"cpu_stub_returns_zeroed_spectrum_layout", &test_cpu_stub_returns_zeroed_spectrum_layout},
         {"engine_rejects_null_samples_for_nonzero_batch",
          &test_engine_rejects_null_samples_for_nonzero_batch},
         {"engine_rejects_zero_topk_in_topk_mode", &test_engine_rejects_zero_topk_in_topk_mode},
         {"engine_cpu_route_uses_cpu_backend", &test_engine_cpu_route_uses_cpu_backend},
-        {"engine_auto_route_falls_back_to_cpu", &test_engine_auto_route_falls_back_to_cpu},
+        {"engine_auto_route_falls_back_to_cpu_scalar",
+         &test_engine_auto_route_falls_back_to_cpu_scalar},
         {"engine_gpu_route_throws_when_unavailable",
          &test_engine_gpu_route_throws_when_unavailable},
+        {"cpu_avx2_route_throws_when_unimplemented",
+         &test_cpu_avx2_route_throws_when_unimplemented},
+        {"cpu_avx512_route_throws_when_unimplemented",
+         &test_cpu_avx512_route_throws_when_unimplemented},
     };
 
     for (const TestCase& test : tests) {
