18244d24aa0dbda96c650cf051de07cdbca0ad4f
MortimerDe <tomas.howard.suffolk@gmail.com>
2026-05-09 03:00:08 +0800

    add island model header

---
+++ include/bmmpy/ga/island_model.hpp
@@ -0,0 +1,43 @@
+// include/bmmpy/ga/island_model.hpp
+#pragma once
+
+#include "bmmpy/core/row_window.hpp"
+#include "bmmpy/ga/island.hpp"
+#include "bmmpy/ga/types.hpp"
+
+#include <memory>
+#include <vector>
+
+namespace bmmpy::ga {
+
+struct IslandModelConfig {
+    std::size_t island_count = 8;
+    MigrationPolicy migration;
+};
+
+class IslandModel final {
+public:
+    IslandModel(IslandModelConfig config, std::unique_ptr<Island> prototype);
+    IslandModel(IslandModelConfig config, std::vector<std::unique_ptr<Island>> workers);
+
+    void initialize(const ::bmmpy::RowWindow& window);
+
+    void start();
+    void request_stop() noexcept;
+    void wait();
+
+    bool running() const noexcept;
+    bool stop_requested() const noexcept;
+
+    Individual best_individual() const;
+    IslandModelSnapshot snapshot() const;
+
+    Individual run(const ::bmmpy::RowWindow& window);
+
+    const char* name() const noexcept { return "island_model"; }
+
+private:
+    IslandModelConfig _config;
+};
+
+} // namespace bmmpy::ga
