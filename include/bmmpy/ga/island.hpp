d31889eafa3efdf980d740a83e4697fbe5201e43
MortimerDe <tomas.howard.suffolk@gmail.com>
2026-05-08 23:34:02 +0800

    add island (single worker) header

---
+++ include/bmmpy/ga/island.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "bmmpy/core/row_window.hpp"
+#include "bmmpy/ga/types.hpp"
+
+#include <memory>
+
+namespace bmmpy::ga {
+class Island {
+public:
+    virtual ~Island() = default;
+    virtual std::unique_ptr<Island> clone() const = 0;
+    virtual void initialize(const ::bmmpy::RowWindow& window) = 0;
+    virtual void step_generation() = 0;
+    virtual bool done() const noexcept = 0;
+    virtual std::size_t generation() const noexcept = 0;
+    virtual Individual best_individual() const = 0;
+    virtual RunStats stats() const = 0;
+    virtual std::vector<Individual> export_migrants(std::size_t max_count) = 0;
+    virtual void import_migrants(std::vector<Individual> migrants) = 0;
+    virtual const char* name() const noexcept = 0;
+};
+} // namespace bmmpy::ga
