9fef4985e37af95e28925d127ac62b495a0280c3
MortimerDe <tomas.howard.suffolk@gmail.com>
2026-05-09 18:24:04 +0800

    couple naming fixes

--- include/bmmpy/ga/island_model.hpp
+++ include/bmmpy/ga/island_model.hpp
@@ -28,9 +28,14 @@ public:
 
     void initialize(const ::bmmpy::RowWindow& window);
 
+    // start() -> working... -> request_stop() -> wait() means: "start the optimization, and when
+    // you can, stop it as soon as possible, and wait until it's fully stopped start() -> wait()
+    // means: "wait until the optimization is fully done, then return" request_stop() without wait()
+    // means: "initialize the stopping process, but don't block immediately, you can check the
+    // status with running() and stop_requested()"
     void start();
-    void request_stop() noexcept;
-    void wait();
+    void request_stop() noexcept; // please, begin stopping the optimization asap
+    void wait();                  // lock until system is fully stopped
 
     bool running() const noexcept;
     bool stop_requested() const noexcept;
@@ -38,7 +43,7 @@ public:
     Individual best_individual() const;
     IslandModelSnapshot snapshot() const;
 
-    Individual optimize(const ::bmmpy::RowWindow& window);
+    Individual run_to_completion(const ::bmmpy::RowWindow& window);
 
     const char* name() const noexcept { return "island_model"; }
 
